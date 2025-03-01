8111bc5bea6aea483e33c9a243dfaf9b11d24804
abdul <theasamad6@gmail.com>
2025-03-01 15:22:05 +0500

    improves code used validations

---
+++ venv/prac.py
@@ -0,0 +1,13 @@
+
+
+# list1 = [("usd", 10), ("eur", 5), ("jpy", 15), ("gbp", 3), ("aud", 7)]
+
+# # Sort based on the second element (value)
+# top_3 = sorted(list1, key=lambda x: x[1], reverse=True)[:3]
+# bottom_3 = sorted(list1, key=lambda x: x[1])[:3]
+
+# print("\n=== Top 3 Best Performing Currencies ===")
+# print(top_3)  # Output: [('jpy', 15), ('usd', 10), ('aud', 7)]
+
+# print("\n=== Bottom 3 Worst Performing Currencies ===")
+# print(bottom_3)  # Output: [('gbp', 3), ('eur', 5), ('aud', 7)]
